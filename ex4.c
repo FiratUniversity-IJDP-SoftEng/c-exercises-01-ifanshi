74e7a8e76053fff3e2c0a3727e71e4e1a9c167a9
Muhammed Ali İğdeli <muhammedaligdeli@gmail.com>
2025-10-29 01:31:39 +0300

    Update ex4.c

--- ex4.c
+++ ex4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Counting from 2 to 10 by twos:\n");
+
+    for (int i = 2; i <= 10; i += 2)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
 
