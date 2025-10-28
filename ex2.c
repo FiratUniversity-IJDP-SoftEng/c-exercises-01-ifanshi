583acda6ffc03d0ec927856216f7e5c0856ae3d8
Muhammed Ali İğdeli <muhammedaligdeli@gmail.com>
2025-10-29 01:28:59 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age;
+
+    // kullanıcıdan yaşını al
+    printf("What is your age? ");
+    scanf("%d", &age);
+
+    // bir for döngüsü kullanarak yaş kadar "Happy Birthday!" yazdır
+    for (int i = 0; i < age; i++)
+    {
+        printf("Happy Birthday!\n");
+    }
+    return 0;
+}
 
