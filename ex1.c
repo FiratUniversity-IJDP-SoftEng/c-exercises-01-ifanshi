50518dc7fb999d2b70df1f450fb4bbdd26fb2c3f
Muhammed Ali İğdeli <muhammedaligdeli@gmail.com>
2025-10-29 01:27:23 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main(void)
+{
+    // The secret number is set to 3
+    int secret_number = 3;
+    int guess;
+
+    // Get the user's guess
+    printf("Guess a number between 1 and 5: ");
+    scanf("%d", &guess);
+
+    // Check the guess
+    if (guess == secret_number)
+    {
+        printf("You got it right\n");
+    }
+    else
+    {
+        printf("Sorry that was wrong The secret number was %d\n", secret_number);
+    }
+
+    return 0;
+}
