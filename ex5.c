fb93247381db5d27b1baa61c9408b6c4cd951008
Muhammed Ali İğdeli <muhammedaligdeli@gmail.com>
2025-10-29 01:32:15 +0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+
+void sayHello(char name[])
+{
+    printf("Hello, %s!\n", name);
+}
+
+int main(void)
+{
+    char user_name[50]; // İsim için 50 karakterlik bir yer ayıralım
+
+    printf("What is your name? ");
+    scanf("%s", user_name);
+
+    sayHello(user_name);
+
+    return 0;
+}
 
